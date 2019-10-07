2fb90cc8a576de7941585ceec53ee06c82993fbd
cns2382 <csanord@austin.utexas.edu>
2019-10-07 17:37:22 -0500

    Create react app, get rid of unnecessary files, stub out very basic landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Clarks's Queer Book Club ATX
+        </h1>
       </header>
+      <body>
+        <a href="#">Create an Event</a>
+        <h2>Upcoming Events</h2>
+
+        <div id="event-card">
+          <h3>November Event: Conundrum</h3>
+          <img src="#" alt="Conundrum Cover" />
+        </div>
+      </body>
     </div>
   );
 }
