2fa06e85335c966fd41c2067423c407eba0aa61d
Clark Sanford <clrksanford@gmail.com>
2019-11-11 22:41:41 -0600

    Don't show rsvp button for past events

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,9 +5,14 @@ export default (props) => {
     return(
         <div className="event-card" key={props.event.id}>
           <h3>{props.event.title}</h3>
-          <button
-            onClick={() => props.rsvpClick(props.event.id)}
-          >RSVP</button>
+          {props.rsvpClick ?
+            <button
+              onClick={() => props.rsvpClick(props.event.id)}
+            >
+              RSVP
+            </button> : ''
+          }
+
           <p>Date: {props.event.dateFormatted} @ 7pm</p>
           <p>Book: <em>{props.event.book.title}</em> by {props.event.book.author}</p>
           <img src={props.event.book.cover_url} alt={props.event.book.title + " Cover"} />
